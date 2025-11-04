933c6315463918bb0e4ccb794087b81b5bc790c1
user <user@10.29.87.165>
2025-11-04 15:51:58 +0900

    java導入

--- Sekiguchi_Makoto/src/curriculum.java
+++ Sekiguchi_Makoto/src/curriculum.java
@@ -4,6 +4,21 @@ public class curriculum {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World");
+		
+		//自己紹介
+		String name = "関口　誠";
+		int age = 22;
+		String hobby = "冒険";
+		//自己紹介文の出力
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(hobby);
+		//出力コードのショートカットsysout
+		System.out.println();
+		//1行コメント
+		/*この囲いの中のみコメント化可能。複数行
+		 にかけてコメントするときに便利*/
+		
 	}
 
 }
