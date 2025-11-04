8ca5cc52864907a67bb1ce6325712e1f410372e0
user <user@10.29.87.165>
2025-11-04 17:14:55 +0900

    javaレッスン変数①とQ2までの課題

--- Sekiguchi_Makoto/src/curriculum.java
+++ Sekiguchi_Makoto/src/curriculum.java
@@ -13,15 +13,28 @@ public class curriculum {
 		System.out.println(name);
 		System.out.println(age);
 		System.out.println(hobby);
-		//出力コードのショートカットsysout
+		
+		//学んだこと：出力コードのショートカットsysout
 		System.out.println();
-		//1行コメント
+		
+		//学んだこと：1行コメントと複数行コメントのやり方
 		/*この囲いの中のみコメント化可能。複数行
 		 にかけてコメントするときに便利*/
 		//ここまでがjava2.4レッスン
 		
-		//javaレッスン2.5任意の文字をコンソールに表示する
+		//javaレッスン2.5
+		//学んだこと：任意の文字をコンソールに表示する
 		System.out.println("任意の文字を表示する。");
+		
+		//javaレッスン2.7変数①
+		/*学んだこと：定数はデータ型の直前にfinalを書く。変数と異なり後からの変更ができない。*/
+		final int number = 10;
+		//number = 5;と書くとエラーになる。
+		System.out.println(number);
+		int num2 = 15;
+		num2 = 20;
+		System.out.println(num2);
+		
 		
 	}
 
