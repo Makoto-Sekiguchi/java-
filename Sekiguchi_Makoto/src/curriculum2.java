97abacf7cbe7e540be8042f7b5e2d456e7548f71
user <user@10.29.87.165>
2025-11-05 14:15:21 +0900

    課題Q１の提出

---
+++ Sekiguchi_Makoto/src/curriculum2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class curriculum2 extends curriculum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int score = 75;
+		if (score >= 60) {
+			System.out.println("合格です！");
+	     }
+	}
+}
