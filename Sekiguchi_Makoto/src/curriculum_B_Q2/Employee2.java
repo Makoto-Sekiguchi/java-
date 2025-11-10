cbb1beb4288f0d751547bc65bc6312fa0f760a9f
user <user@10.29.87.165>
2025-11-10 16:27:04 +0900

    課題提出：javaカリキュラム５-Q2

---
+++ Sekiguchi_Makoto/src/curriculum_B_Q2/Employee2.java
@@ -0,0 +1,12 @@
+package curriculum_B_Q2;
+
+public class Employee2 {
+	
+	//
+	String employeeld;
+	String name;
+	public void showinfo(){//public:同フォルダ内の別ファイルからのアクセスを可能にする
+		System.out.println("社員ID：" + employeeld + ", " + "名前：" + name);
+		
+	}
+}
