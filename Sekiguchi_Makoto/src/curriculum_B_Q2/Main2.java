11303a48b76b1cf153b65c871cb7ae7e075647ec
user <user@10.29.87.165>
2025-11-10 16:26:10 +0900

    課題提出：javaカリキュラム５-Q2

---
+++ Sekiguchi_Makoto/src/curriculum_B_Q2/Main2.java
@@ -0,0 +1,14 @@
+package curriculum_B_Q2;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Employee2 employee = new Employee2();
+		
+		employee.employeeld = "E001";
+		employee.name = "佐藤太郎";
+		employee.showinfo();
+	}
+
+}
